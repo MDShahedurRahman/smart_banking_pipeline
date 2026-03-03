4a26265f130068180358c8328fa4bf713570c223
Md Shahedur Rahman <mr6139@nyu.edu>
2026-03-02 23:22:24 -0500

    Create config module

---
+++ config.py
@@ -0,0 +1,10 @@
+SOURCE_FILE = "data/transactions.csv"
+
+BRONZE_PATH = "output/bronze/"
+SILVER_PATH = "output/silver/"
+FRAUD_PATH = "output/fraud/"
+GOLD_PATH = "output/gold/"
+REPORT_PATH = "output/reports/"
+
+HIGH_AMOUNT_THRESHOLD = 10000
+FRAUD_RISK_THRESHOLD = 2
