6654f63d2a722e318b60c40c58a94ee100c78015
Md Shahedur Rahman <mr6139@nyu.edu>
2026-03-02 23:41:52 -0500

    Add null validation

--- jobs/silver_transformation.py
+++ jobs/silver_transformation.py
@@ -7,4 +7,12 @@ from utils.validators import validate_transactions
 def run_silver_job(spark):
     print("Running Silver Transformation...")
 
+    ensure_dir(SILVER_PATH)
+
+    df = spark.read.parquet(BRONZE_PATH)
+
+    cleaned = (
+        validate_transactions(df)
+    )
+
     return cleaned
