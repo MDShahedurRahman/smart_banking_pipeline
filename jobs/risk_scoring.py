60f89bc845419f83bba5f5d02725bdb07ef9aaa3
Md Shahedur Rahman <mr6139@nyu.edu>
2026-03-05 04:07:20 -0500

    Create risk scoring module

---
+++ jobs/risk_scoring.py
@@ -0,0 +1,9 @@
+from pyspark.sql.functions import when, col
+from config import FRAUD_PATH, SILVER_PATH
+from config import FRAUD_RISK_THRESHOLD
+
+
+def run_risk_scoring(spark):
+    print("Running Risk Scoring...")
+
+    return scored
